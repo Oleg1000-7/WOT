7f4911666713db11ee9bbbc01ce70eacac402efb
Oleg <oalzeg09@gmail.com>
2025-05-13 18:38:59 +0300

    register password fix

--- forms/register_form.py
+++ forms/register_form.py
@@ -1,12 +1,13 @@
 from flask_wtf import FlaskForm
 from wtforms import PasswordField, StringField, SubmitField, EmailField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length
 
 
 class RegisterForm(FlaskForm):
     name = StringField('Имя пользователя', validators=[DataRequired()])
     email = EmailField('Почта', validators=[DataRequired()])
     password = PasswordField('Пароль', validators=[DataRequired()])
-    password_again = PasswordField('Повторите пароль', validators=[DataRequired()])
+    password_again = PasswordField('Повторите пароль',
+                                   validators=[DataRequired(), Length(min=6, message="Пароль слишком короткий")])
 
     submit = SubmitField('Зарегистрироваться')
